9801761f63b2139ad0cb117092847bffca32e370
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-21 18:41:56 -0300

    Add files via upload

---
+++ modules/cadastro.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from database 
+import inserir_cliente, buscar_cliente_por_id,inserir_racha_financeiro
+import streamlit.components.v1 as components
+ 
+def show():
+    if not st.session_state.get("logado", False):
+        st.warning("Acesso negado. Faça login para continuar.")
+        st.stop()
+
+    st.subheader("Cadastro de Cliente")
+
+    # --- FORMULÁRIO DE CADASTRO ---
+    with st.form("form_cadastro"):
+        nome     = st.text_input("Nome", max_chars=100)
+        login  = st.text_input("Apelido", max_chars=100)
+        
+        col1, col2 = st.columns([1, 4])
+        with col1:
+            ddd = st.text_input("DDD", max_chars=2)
+        with col2:
+            telefone = st.text_input("Telefone", max_chars=12)
+            
+        # Mapeia o texto visível para o valor interno
+        opcoes = {
+            "Normal": "N",
+            "Dispensado": "D"
+        }
+
+        # Selectbox exibindo só os textos
+        opcao_escolhida = st.selectbox("Selecione o Tipo de Status do Jogador:", list(opcoes.keys()))
+
+        # Pega o valor real que você quer usar (N ou D)
+        StatusdePagamento = opcoes[opcao_escolhida]
+        
+        botao_cadastrar = st.form_submit_button("Cadastrar")
+
+    if botao_cadastrar:
+        if nome.strip() == ""or  login.strip() == "":
+            st.error("O campo 'Nome/Apelido' são obrigatório.")
+        else:
+            cliente_id = inserir_cliente(nome,login,ddd,telefone,StatusdePagamento)
+            if cliente_id:
+                st.success(f"Jogador  '{nome}' Cadastrado com sucesso!)")
+
+                # Buscar cliente e mostrar após o form
+                cliente = buscar_cliente_por_id(cliente_id)
+                if cliente:
+                    inserir_racha_financeiro(cliente_id)# grava o cliente_id na session
+                  
+ 
+            else:
+                st.error("Erro ao cadastrar o cliente.")
---
+++ modules/despesa.py
@@ -0,0 +1,79 @@
+import streamlit as st
+from database 
+import buscar_logins,buscar_anos,buscar_meses,buscar_dados_filtrados,buscar_logins,atualizar_valor_despesa,buscar_usuario_por_seq,buscar_usuario_por_seq1,listarpagamento
+from datetime 
+import datetime
+import pandas as pd
+ 
+st.subheader("Financeiro")
+ 
+
+
+def show():
+    
+    # Pegar mês e ano atual
+    mes_atual = datetime.now().month
+    ano_atual = datetime.now().year
+
+    # Listas de opções
+    meses = list(range(1, 13))
+    anos = list(range(2025, 2031))
+    
+    # Carregar opções 
+    meses = buscar_meses()
+    anos = buscar_anos()
+ 
+
+    
+            
+    # Buscar os dados
+    dados_logins = buscar_logins()  # Lista de tuplas: (Seq, Login, Nome)
+
+    # Formata" ",r as opções para exibição
+    opcoes =  [ " ","Lavagem dos Coletes", "Compra de Bola", "Compra de Coletes", "Material de Farmácia"]
+    # Criar o selectbox
+    selecionado = st.selectbox("Selecione o Jogador:", opcoes)
+
+    # Recuperar os dados reais com base na seleção
+    tipoDespesa =  (selecionado)
+   
+
+    # Criar colunas para mês e ano
+    col1, col2, _ = st.columns([2, 4, 6])
+    with col1:
+        mes = st.selectbox("Mês", meses, index=meses.index(mes_atual))
+    with col2:
+        if ano_atual in anos:
+            ano = st.selectbox("Ano", anos, index=anos.index(ano_atual))
+        else:
+            ano = st.selectbox("Ano", anos)
+
+    # Input do valor
+    valor = st.number_input("Digite o Valor da Despesa  :", min_value=0.0)
+    
+    descricao = st.text_input("Descrição", max_chars=100)
+
+    if st.button("Efetuar o pagamento"):
+        atualizar_valor_despesa(mes, ano, tipoDespesa, descricao , valor)
+        st.success("✅ Pagamento efetuado com sucesso!")
+        
+        usuario = listarpagamento() 
+ 
+        # Converte lista de tuplas em DataFrame
+        df_usuario = pd.DataFrame( usuario,columns=["Mes", "Ano","Valor", "tipodespesa","Descricao"])
+
+        # Formata ValorPago como moeda brasileira
+        df_usuario["Valor"] = df_usuario["Valor"].apply(
+            lambda x: f"R$ {x:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+        )
+
+        # Remove o índice completamente
+        st.dataframe(df_usuario.reset_index(drop=True), use_container_width=True)
+
+ 
+
+        
+
+ 
+ 
+         
