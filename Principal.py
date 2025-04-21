7359d407f27c405df1abb8ac7f22040af424f8d0
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-21 18:03:21 -0300

    Update Principal.py
    sadasd

--- Principal.py
+++ Principal.py
@@ -1,28 +1,5 @@
 import streamlit as st
-from modules import principal, cadastro, financeiro, pagamento,despesa
-import header  # novo módulo com o cabeçalho
 
-if "usuario" not in st.session_state:
-    st.session_state.usuario = ""
-    
 def show():
-    # Mostra o cabeçalho
-    header.show()
-
-    # Menu lateral
-    menu = st.sidebar.radio("Menu", ["Principal", "Cadastro", "Financeiro", "Pagamento", "Despesa", "Sair"])
-
-    if menu == "Principal":
-        principal.show()
-    elif menu == "Cadastro":
-        cadastro.show()
-    elif menu == "Financeiro":
-        financeiro.show()
-    elif menu == "Pagamento":
-        pagamento.show()
-    elif menu == "Despesa":
-        despesa.show()
-    elif menu == "Sair":
-        st.session_state.logado = False
-        st.session_state.usuario = ""
-        st.rerun()
+    st.subheader("Página Principal")
+    st.write("Aqui vai o formulário de cadastro.")
