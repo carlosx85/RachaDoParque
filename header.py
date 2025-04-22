181fc7ca0d0a99dd8581db2eaa901b1d172f6212
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-21 21:52:26 -0300

    Add files via upload

---
+++ header.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import base64
+
+if "usuario" not in st.session_state:
+    st.session_state.usuario = ""
+
+ 
+def showx():
+    header_html = f"""
+    <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
+        <img src="https://boladecapotao.com/img/Racha_Logo_P.png" width="50" style="margin-right: 15px;">
+        <h5 style="margin: 0;">Racha do Parque  ({st.session_state.usuario})</h5>
+    </div>
+    <hr style="margin-top: 5px; margin-bottom: 15px;">
+    """
+    st.markdown(header_html, unsafe_allow_html=True)
