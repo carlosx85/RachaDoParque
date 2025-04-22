15bf1e96f2ae4abd7c6a1b79024a254f7eacb4a2
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-22 12:00:41 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.write("Host do MySQL:", st.secrets["mysql"]["host"])
-st.write("Host do MySQL:", st.secrets["mysql"]["user"])
-st.write("Host do MySQL:", st.secrets["mysql"]["umotimoempreen02"])
+st.write("Host do MySQLuser:", st.secrets["mysql"]["user"])
+st.write("Host do MySQL:", st.secrets["mysql"]["database"])
