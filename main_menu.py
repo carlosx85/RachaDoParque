a08e8ed2c3d560068d8b46d0ac659dd2a8902d33
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-21 22:30:35 -0300

    Add files via upload

--- main_menu.py
+++ main_menu.py
@@ -2,7 +2,7 @@ import streamlit as st
 from modules.principal import show as principal_show 
 
 from modules.financeiro import show as financeiro_show
-from modules.pagamento import show as pagamento_show
+
 from modules.despesa import show as despesa_show
  
 import header  # se tiver
