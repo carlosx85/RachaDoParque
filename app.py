9910cd4e327cf3229f8588aece35c84f7a960e65
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-22 12:15:54 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,40 @@
 import streamlit as st
+from database import validar_login
+import main_menu
+ 
+ 
 
-st.write("Host do MySQL:", st.secrets["mysql"]["host"])
-st.write("Host do MySQLuser:", st.secrets["mysql"]["user"])
-st.write("Host do MySQL:", st.secrets["mysql"]["database"])
+# Inicializa variáveis da sessão, se ainda não existirem
+if "usuario" not in st.session_state:
+    st.session_state.usuario = ""
+
+if "logado" not in st.session_state:
+    st.session_state.logado = False
+
+# Se estiver logado, mostra o menu principal
+if st.session_state.logado:
+    main_menu.show()
+
+# Caso contrário, mostra tela de login
+else:
+    st.markdown(
+        """
+        <div style="text-align: center;">
+            <img src="https://boladecapotao.com/img/Racha_Logo_G.png" width="150">
+            <p style="font-size: 12px; margin-top: 5px;">Racha do Parque</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+    usuario = st.text_input("Usuário")
+    senha = st.text_input("Senha", type="password")
+
+    if st.button("Entrar"):
+        if validar_login(usuario, senha):
+            st.session_state.logado = True
+            st.session_state.usuario = usuario 
+            st.session_state.senha = senha 
+            st.rerun()
+        else:
+            st.error("Usuário ou senha inválido")
