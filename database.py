69bd5e3af7eab31a8cd54d38c45698362fbb370e
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-22 11:11:16 -0300

    Add files via upload

--- database.py
+++ database.py
@@ -19,7 +19,7 @@ def validar_login(usuario, senha):
     try:
         conexao = conectar()
         cursor = conexao.cursor(buffered=True)
-        consulta = "SELECT * FROM Racha_Usuario WHERE Login like %s "
+        consulta = "SELECT * FROM Racha_Usuario   "
         cursor.execute(consulta, (usuario, senha))
         resultado = cursor.fetchone()
         return resultado is not None
