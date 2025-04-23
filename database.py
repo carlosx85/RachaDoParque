fd4015558012dbc7a3f4d543d11a1339bf7700ed
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-23 17:31:00 -0300

    Add files via upload

--- database.py
+++ database.py
@@ -19,7 +19,7 @@ def validar_login(usuario, senha):
     conexao = conectar()
     cursor = conexao.cursor(buffered=True)  # <-- corrigido aqui
     consulta = "SELECT * FROM Racha_Usuario WHERE Login = %s AND Senha = %s"
-    cursor.execute(consulta, (usuario, senha))
+    cursor.execute(consulta, (usuario,))
     resultado = cursor.fetchone()
     cursor.close()
     conexao.close()
