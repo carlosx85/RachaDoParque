f86686e95f74928f4ebc7e7c5225c8a7e776a841
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-24 11:43:36 -0300

    Add files via upload

--- modules/principal.py
+++ modules/principal.py
@@ -1,24 +1,14 @@
 import streamlit as st
 
 
-Receita = 350
-Despesa = 150
+Receita = 350.00
+Despesa = 150.00
+Saldo = Receita - Despesa
 
-Saldo= Receita - Despesa
-
-def show():
-    st.subheader("Resumo Geral")    
-    
-    col1, col2 = st.columns(2)
-
-    with col1:  
-        st.write("Receita",f"{Receita}")
-        st.write("Despesa",f"{Despesa}")
-
-
-    with col2:
-        st.write("Produção - 12/2022",f"{Saldo}")
-  
+# Exibe valores formatados em reais
+st.write("Receita:", f"R$ {Receita:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+st.write("Despesa:", f"R$ {Despesa:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+st.write("Saldo:", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
 
 
     
