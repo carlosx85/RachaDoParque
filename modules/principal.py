6f3ef9ac961979f4d2d2f83afcb6bd6654ee23b8
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-24 11:59:43 -0300

    Add files via upload

--- modules/principal.py
+++ modules/principal.py
@@ -6,15 +6,6 @@ def show():
     Receita = 350.00
     Despesa = 150.00
     Saldo = Receita - Despesa
-
-    # Exibe valores formatados em reais
-    st.write("Receita:", f"R$ {Receita:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
-    st.write("Despesa:", f"R$ {Despesa:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
-    st.write("Saldo:", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
-
-  
-
-    
     
 
     # Mostrando como métrica
