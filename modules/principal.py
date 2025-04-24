2661cb9771a75f71446b3f2b675831bab2beb274
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-24 12:41:44 -0300

    Add files via upload

--- modules/principal.py
+++ modules/principal.py
@@ -22,4 +22,30 @@ def show():
     
     
     
-    
+
+    def formatar(valor):
+        return f"R$ {valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+
+
+    # Define cor visual com truque de delta
+    if Saldo > 0:
+        delta = "+1"
+        cor_saldo = "normal"   # Verde
+    elif Saldo < 0:
+        delta = "-1"
+        cor_saldo = "inverse"  # Vermelho
+    else:
+        delta = "0"
+        cor_saldo = "off"      # Cinza
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.metric("📈 Receita", formatar(Receita), border=True)
+
+    with col2:
+        st.metric("📉 Despesa", formatar(Despesa), border=True)
+
+    with col3:
+        st.metric("💰 Saldo", formatar(Saldo), delta=delta, delta_color=cor_saldo, border=True)
+
