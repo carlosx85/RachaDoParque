63da66befe5ca4c854437679f224ad31ef357d4a
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-24 11:32:57 -0300

    Add files via upload

--- modules/principal.py
+++ modules/principal.py
@@ -6,15 +6,15 @@ def show():
     
     
     
-col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2)
 
-with col1:  
+    with col1:  
+        st.header("Produção - 12/2022")
+
+
+    with col2:
     st.header("Produção - 12/2022")
-
-
-with col2:
-   st.header("Produção - 12/2022")
-   st.metric("",f"{2,2:}", "")  
+    st.metric("",f"{2,2:}", "")  
 
 
     
