65b2181b70a78234db138e5c40d33ff439525140
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-24 12:26:53 -0300

    Add files via upload

--- modules/principal.py
+++ modules/principal.py
@@ -3,38 +3,20 @@ import streamlit as st
 
  
 def show():
-    Receita = 35.00
+    Receita = 350.00
     Despesa = 150.00
     Saldo = Receita - Despesa
     
 
     # Mostrando como métrica
-# Formatação
-    def formatar(valor):
-        return f"R$ {valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
-
-    # Definir cor via delta_color
-    if Saldo > 0:
-        cor_saldo = "normal"  # verde
-        delta = "+1"           # qualquer positivo simula verde
-    elif Saldo < 0:
-        cor_saldo = "inverse"  # vermelho
-        delta = "-1"
-    else:
-        cor_saldo = "off"      # cinza
-        delta = "0"
-
-    col1, col2, col3 = st.columns(3)
+    st.metric(
+        label="💰 Saldo",
+        value=f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+)
+    c, d = st.columns(2)
+    c.metric("Receita", f"R$ {Receita:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
+    d.metric("Despesa", f"R$ {Despesa:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
     
-    with col1:
-        st.metric("💰 Saldo", formatar(Saldo), delta=delta, delta_color=cor_saldo)
-
-    with col2:
-        st.metric("📈 Receita", formatar(Receita), delta=delta, delta_color=cor_saldo)
-
-    with col3:
-        st.metric("📉 Despesa", formatar(Despesa), delta=delta, delta_color=cor_saldo)
-
-
-
+    
+    
     
