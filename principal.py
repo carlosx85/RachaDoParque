ecee40e5474c0b3a72fdd88c2c76909e7a7224f1
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-24 11:30:45 -0300

    Add files via upload

---
+++ principal.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def show():
+    st.subheader("Resumo Geral")
+    st.write("Aqui vai o formulário de cadastro.")
+    
+    
+    
+col1, col2 = st.columns(2)
+
+with col1:  
+    st.header("Produção - 12/2022")
+
+
+with col2:
+   st.header("Produção - 12/2022")
+   st.metric("",f"{2,2:}", "")  
+
+
+    
+    
+    
+    
