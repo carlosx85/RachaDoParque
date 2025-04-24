72d62e7c2e6f23e218725ecdc6d9f3cfc83d4935
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-24 11:52:54 -0300

    Add files via upload

--- modules/principal.py
+++ modules/principal.py
@@ -8,13 +8,20 @@ def show():
     Saldo = Receita - Despesa
 
     # Exibe valores formatados em reais
-    st.write("Receita:", f"R$ {Receita:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
-    st.write("Despesa:", f"R$ {Despesa:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
-    st.write("Saldo:", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+    st.write("Receita:", f"R$ {Receita:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
+    st.write("Despesa:", f"R$ {Despesa:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
+    st.write("Saldo:", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
 
   
 
+    a, b = st.columns(2)
+    c, d = st.columns(2)
 
+    a.metric("Temperature", "30°F", "-9°F", border=True)
+    b.metric("Wind", "4 mph", "2 mph", border=True)
+
+    c.metric("Humidity", "77%", "5%", border=True)
+    d.metric("Pressure", "30.34 inHg", "-2 inHg", border=True)
     
     
     
