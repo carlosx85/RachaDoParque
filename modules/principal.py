7c605e2ede0eb811d9ee587beffb6d80cffdad13
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-24 11:56:57 -0300

    Add files via upload

--- modules/principal.py
+++ modules/principal.py
@@ -14,14 +14,13 @@ def show():
 
   
 
-    a, b = st.columns(2)
-    c, d = st.columns(2)
+    a = st.columns(1)
+    c, d = st.columns(1)
 
-    a.metric("Temperature", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
-    b.metric("Wind", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
-
-    c.metric("Humidity", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
-    d.metric("Pressure", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , "-2 inHg", border=True)
+    a.metric("Saldo", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
+    
+    c.metric("Receita", f"R$ {Receita:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
+    d.metric("Despesa", f"R$ {Despesa:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
     
     
     
