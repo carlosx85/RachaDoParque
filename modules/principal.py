b8271eb052fb32cc9a3046237cd7ac3141e507bb
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-24 11:39:47 -0300

    Add files via upload

--- modules/principal.py
+++ modules/principal.py
@@ -1,16 +1,23 @@
 import streamlit as st
 
+
+Receita = 350,00
+Despesa = 150,00
+
+Saldo= Receita - Despesa
+
 def show():
     st.subheader("Resumo Geral")    
     
     col1, col2 = st.columns(2)
 
     with col1:  
-        st.write("Produção - 12/2022")
+        st.write("Receita",f"{Receita:}")
+        st.write("Despesa",f"{Despesa:}")
 
 
     with col2:
-        st.write("Produção - 12/2022",f"{2,2:}")
+        st.write("Produção - 12/2022",f"{Saldo:}")
   
 
 
