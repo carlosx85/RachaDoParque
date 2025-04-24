aaca6dc41e0ea01cd54ddf7ca87a7084d0234679
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-24 11:54:37 -0300

    Add files via upload

--- modules/principal.py
+++ modules/principal.py
@@ -17,11 +17,11 @@ def show():
     a, b = st.columns(2)
     c, d = st.columns(2)
 
-    a.metric("Temperature", "30°F", "-9°F", border=True)
-    b.metric("Wind", "4 mph", "2 mph", border=True)
+    a.metric("Temperature", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
+    b.metric("Wind", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
 
-    c.metric("Humidity", "77%", "5%", border=True)
-    d.metric("Pressure", "30.34 inHg", "-2 inHg", border=True)
+    c.metric("Humidity", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , border=True)
+    d.metric("Pressure", f"R$ {Saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".") , "-2 inHg", border=True)
     
     
     
