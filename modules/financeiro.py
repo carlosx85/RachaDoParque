a296321d73a5431a003fd937a9fb8e6d3d7166ae
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-25 18:42:43 -0300

    Add files via upload

--- modules/financeiro.py
+++ modules/financeiro.py
@@ -41,6 +41,8 @@ def show():
 
             st.success(f"{len(dados)} registro(s) encontrado(s).")
             
+           
+            
 
             for i, item in enumerate(dados, start=1):
                 seq = item.get("Seq", "—")
@@ -53,6 +55,16 @@ def show():
                 situacao = "✅" if pago == "Pago" else "🕓" if pago == "Em Negociacao" else "❌"
  
                 obs_str = f"({obs})" if obs not in [None, "None", ""] else ""
+                
+                header_html = f"""
+                <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
+                    <img src="https://boladecapotao.com/img/Racha_Logo_P.png" width="50" style="margin-right: 15px;">
+                    <h5 style="margin: 0;">Racha do Parque  ({st.session_state.usuario})</h5>
+                </div>
+                <hr style="margin-top: 5px; margin-bottom: 15px;">
+                """
+                st.markdown(header_html, unsafe_allow_html=True)
+                
                 
                 st.markdown(f"""
                     <div style="font-size: 13px;">
