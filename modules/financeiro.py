b656dd748fd45dd0074aa9d441fc43c8b7d0e10c
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-25 18:43:51 -0300

    Add files via upload

--- modules/financeiro.py
+++ modules/financeiro.py
@@ -56,14 +56,7 @@ def show():
  
                 obs_str = f"({obs})" if obs not in [None, "None", ""] else ""
                 
-                header_html = f"""
-                <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
-                    <img src="https://boladecapotao.com/img/Racha_Logo_P.png" width="50" style="margin-right: 15px;">
-                    <h5 style="margin: 0;">Racha do Parque  ({st.session_state.usuario})</h5>
-                </div>
-                <hr style="margin-top: 5px; margin-bottom: 15px;">
-                """
-                st.markdown(header_html, unsafe_allow_html=True)
+
                 
                 
                 st.markdown(f"""
