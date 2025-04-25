93a4c91da97978fbbc7ffc54deeb1b3643243f6e
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-25 18:46:25 -0300

    Add files via upload

--- modules/financeiro.py
+++ modules/financeiro.py
@@ -32,6 +32,19 @@ def show():
         ano = st.selectbox("Ano", anos, index=anos.index(ano_atual) if ano_atual in anos else 0)
 
     if st.button("🔍 Buscar"):
+        
+        header_html = f"""
+        <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
+            <img src="https://boladecapotao.com/img/Racha_Logo_P.png" width="50" style="margin-right: 15px;">
+            <h5 style="margin: 0;">Racha do Parque  ({st.session_state.usuario})</h5>
+        </div>
+        <hr style="margin-top: 5px; margin-bottom: 15px;">
+        """
+        st.markdown(header_html, unsafe_allow_html=True)       
+        
+        
+        
+        
         try:
             dados = buscar_clientes_por_periodo(ano, mes)
 
