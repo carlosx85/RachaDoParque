adb398c5e61dc8ec61bf48cdc00c8bffee90d377
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-25 18:50:30 -0300

    Add files via upload

--- modules/financeiro.py
+++ modules/financeiro.py
@@ -34,7 +34,7 @@ def show():
     if st.button("🔍 Buscar"):
         
         header_html = f"""
-        <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
+        <div style="display: flex; align-items: center; padding: 5px 0 5px 0;">
             <img src="https://boladecapotao.com/img/Racha_Logo_P.png" width="50" style="margin-right: 15px;">
             <h5 style="margin: 0;">Racha do Parque  (Financeiro)</h5><br>
             <h6 style="margin: 0;">Periodo {mes} {ano}</h6><br>
