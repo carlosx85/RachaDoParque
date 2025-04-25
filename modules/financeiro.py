fff76e48424a07a3f037b17cf0a89b17a9b6eb59
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-25 18:30:24 -0300

    Add files via upload

--- modules/financeiro.py
+++ modules/financeiro.py
@@ -54,7 +54,7 @@ def show():
                 obs_str = f"({obs})" if obs not in [None, "None", ""] else ""
                 
                 st.markdown(f"""
-                    <div style="font-size: 15px;">
+                    <div style="font-size: 14px;">
                         <b>{i}. {login} ({status})</b> {situacao} {formatar_moeda(valor)} {obs_str}
                     </div>
                 """, unsafe_allow_html=True)
