f6e68c962abecc938600fd4d261fcdcfaabe6ebb
Carlos Santos <carlos.santosx85@hotmail.com>
2025-04-30 10:30:52 -0300

    Add files via upload

--- header.py
+++ header.py
@@ -14,3 +14,16 @@ def show():
     <hr style="margin-top: 5px; margin-bottom: 15px;">
     """
     st.markdown(header_html, unsafe_allow_html=True)
+    
+    
+    st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
