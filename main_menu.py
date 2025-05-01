eb9dc7996d5fe1ea0487ea004bfd38be4ee17d38
Carlos Santos <carlos.santosx85@hotmail.com>
2025-05-01 19:33:32 -0300

    Add files via upload

--- main_menu.py
+++ main_menu.py
@@ -12,6 +12,9 @@ if "usuario" not in st.session_state:
 
 def show():
     header.show()
+    
+        # Adicionando imagem na sidebar (antes do menu)
+    st.sidebar.image("https://boladecapotao.com/img/Racha_Logo_G.png", use_column_width=True)
 
     menu = st.sidebar.radio("Menu", ["Principal", "Cadastro", "Financeiro", "Pagamento", "Despesa", "Sair"])
 
@@ -31,15 +34,5 @@ def show():
         st.session_state.usuario = ""
         st.rerun()
         
-        
-        
  
-        header_html = f"""
-        <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
-            <img src="https://boladecapotao.com/img/Racha_Logo_G.png" width="50" style="margin-right: 15px;">
-            <h5 style="margin: 0;">Racha do Parque  ({st.session_state.usuario})</h5>
-        </div>
-        <hr style="margin-top: 5px; margin-bottom: 15px;">
-        """
-        st.markdown(header_html, unsafe_allow_html=True)
 
