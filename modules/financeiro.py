0c32ad9cea5914cc7b305244a43f372408078f34
Carlos Santos <carlos.santosx85@hotmail.com>
2025-05-04 12:46:09 -0300

    Add files via upload

--- modules/financeiro.py
+++ modules/financeiro.py
@@ -43,7 +43,7 @@ def show():
         header_html = f"""
         <div style="display: flex; align-items: center; padding: 5px 0 5px 0;">
             <img src="https://boladecapotao.com/img/Racha_Logo_P.png" width="50" style="margin-right: 13px;">
-            <h5 style="margin: 0;">Racha do Parque ($$$ {mes}/{ano})</h5> 
+            <h5 style="margin: 0;">RDP Financeiro ({mes}/{ano})</h5> 
         
         </div>
         """
